8edbc9ee6ae61249895fdfc3c8ad07b5acff3b8e
Edel <edelmedina322@gmail.com>
2022-09-20 18:30:23 -0300

    Update responsiveness Replace cart img Modify SB

--- src/Components/ShopComponents/StickyBar.js
+++ src/Components/ShopComponents/StickyBar.js
@@ -2,16 +2,14 @@ import "../../Styles/stickybar.css";
 
 const StickyBar = (props) => {
   return (
-    <div class="sticky-bar">
-      <div id="shopping-cart">{props.itemQty}</div>
-      <button
-        onClick={() => {
-          props.checkout();
-        }}
-      >
-        Checkout
-      </button>
-    </div>
+    <button
+      class="sticky-bar"
+      onClick={() => {
+        props.checkout();
+      }}
+    >
+      {props.itemQty}
+    </button>
   );
 };
 
